c84e049aad6ab5225b1adfe7962256671d4e21db
GuolinY <guolin.yang13@gmail.com>
2021-10-18 15:12:18 +0100

    Fixed spacing for links

--- pages/index.js
+++ pages/index.js
@@ -8,11 +8,13 @@ import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import DescriptionIcon from "@mui/icons-material/Description";
 import EmailIcon from "@mui/icons-material/Email";
 import GitHubIcon from "@mui/icons-material/GitHub";
+import { Box } from "@mui/system";
 
 export default function Home() {
   return (
     <Layout>
       <Typography variant="h1">Guolin Yang</Typography>
+
       <Typography variant="h6">
         I'm a third year computing student at{" "}
         <Link href="https://www.imperial.ac.uk/">
@@ -20,36 +22,39 @@ export default function Home() {
         </Link>
       </Typography>
 
-      <Grid
-        container
-        direction="row"
-        justifyContent="center"
-        alignItems="center"
-        spacing={8}
-      >
-        <Grid item>
-          <Link href="https://www.linkedin.com/in/guolinyang13/">
-            <LinkedInIcon fontSize="large" />
-          </Link>
-        </Grid>
+      <Box mt={2}>
+        <Grid
+          item
+          container
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+          spacing={8}
+        >
+          <Grid item>
+            <Link href="https://www.linkedin.com/in/guolinyang13/">
+              <LinkedInIcon fontSize="large" />
+            </Link>
+          </Grid>
 
-        <Grid item>
-          <Link href="/CV.pdf">
-            <DescriptionIcon fontSize="large" />
-          </Link>
-        </Grid>
+          <Grid item>
+            <Link href="/CV.pdf">
+              <DescriptionIcon fontSize="large" />
+            </Link>
+          </Grid>
 
-        <Grid item>
-          <Link href="mailto:guolin.yang13@gmail.com">
-            <EmailIcon fontSize="large" />
-          </Link>
+          <Grid item>
+            <Link href="mailto:guolin.yang13@gmail.com">
+              <EmailIcon fontSize="large" />
+            </Link>
+          </Grid>
+          <Grid item>
+            <Link href="https://github.com/GuolinY">
+              <GitHubIcon fontSize="large" />
+            </Link>
+          </Grid>
         </Grid>
-        <Grid item>
-          <Link href="https://github.com/GuolinY">
-            <GitHubIcon fontSize="large" />
-          </Link>
-        </Grid>
-      </Grid>
+      </Box>
     </Layout>
   );
 }
