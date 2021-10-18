ed9d3d0a1ab4f120072563c2f2a5cce0cc15a70a
GuolinY <guolin.yang13@gmail.com>
2021-10-18 16:45:06 +0100

    optimised imports

--- components/layout.jsx
+++ components/layout.jsx
@@ -1,22 +1,21 @@
 import Head from "next/head";
-import Image from 'next/image'
 import styles from "../styles/Home.module.css";
 
-export default function Layout({title = "Guolin Yang Portfolio", children}) {
-    return (
-        <div className={styles.container}>
-          <Head>
-            <title>{title}</title>
-            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-            <meta charSet="utf-8" />
-            <link rel="icon" href="/favicon.ico" />
-            <link
-              rel="stylesheet"
-              href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-            />
-          </Head>
+export default function Layout({ title = "Guolin Yang Portfolio", children }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>{title}</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta charSet="utf-8" />
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+      </Head>
 
-          <main className={styles.main}>{children}</main>
-        </div>
-      );
+      <main className={styles.main}>{children}</main>
+    </div>
+  );
 }
